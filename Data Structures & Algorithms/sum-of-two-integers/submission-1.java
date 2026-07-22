cbd4a05edfe3d396dbd28a59814b6468f32faa26
Suman Bhunia <sbhunia@nevada.unr.edu>
2026-07-22 11:49:33 -0400

    Update: sum-of-two-integers - submission-1

--- Data Structures & Algorithms/sum-of-two-integers/submission-1.java
+++ Data Structures & Algorithms/sum-of-two-integers/submission-1.java
@@ -1,16 +1,33 @@
 class Solution {
     /*
-    we can use bitwise XOR operation. 
-    first do 0^1^2 .... ^n and store it in res. 
-    then chek all the numers in the array and XOR them with res. 
-    the final res will be the missing number. 
+    bitwise operation has to be performed. 
+
+    Start with LSB for each bit, we need to consider the carry forward. 
+    s = a ^ b ^ c 
+    c = (a & b) | (b & c) | (a & c)
+    now forward that carry to next bit and so on.
     */
-    public int missingNumber(int[] nums) {
-        int res = 0;
-        for(int i=1; i<=nums.length; i++)
-            res = res ^ i;
-        for(int i=0; i< nums.length; i++)
-            res = res^nums[i];
-        return res;
+
+
+
+
+    public int getSum(int a, int b) {
+        
+        int c = 0;
+        int s = 0;
+        for(int i=0; i<32; i++){
+            //shift carry to  one position left
+            c = c<<1;
+
+            // set the ith bit of s according to the forumula 
+            s += (a ^ b ^ c) & (1<<i);  
+
+            // now calculate the carry bit at i 
+            c = ((a & b) | (b & c) | (a & c)) & (1<<i);
+        }
+        if(c !=0){
+            s = ~(s ^ 0xFFFFFFFF);
+        }
+        return s;
     }
 }
